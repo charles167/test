e03f518c276e87d7917badaaa861ce5a07717e60
Ablanya Charles feranmi <charlesablanya@gmail.com>
2025-03-27 17:19:04 +0100

    Fix

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,11 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
+
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
-
-
 
 export const metadata = {
   title: "CharlesDeep",
@@ -18,22 +17,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <AppContextProvider>
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        <Toaster toastOptions={
-          {
-            success:{style:{background:"black",color:"white"}},
-            error:{style:{background:"black",color:"white"} }
-          }
-        } />
-        {children}
-      </body>
-    </html>
-    </AppContextProvider> 
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          <AppContextProvider>
+            <Toaster
+              toastOptions={{
+                success: { style: { background: "black", color: "white" } },
+                error: { style: { background: "black", color: "white" } },
+              }}
+            />
+            {children}
+          </AppContextProvider>
+        </body>
+      </html>
     </ClerkProvider>
-    
   );
 }
